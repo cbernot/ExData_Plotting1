4dab2cb50c789e5691d1021cec421116cdcd2636
cbernot <cbernot2@gmail.com>
2017-02-22 12:12:46 -0500

    update

--- plot3.R
+++ plot3.R
@@ -5,7 +5,7 @@ with(pwrdf,{
   lines(timestamp,Sub_metering_2,col="red")
   lines(timestamp,Sub_metering_3,col="blue")
   legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
-         col=c("black", "red","blue"),lty=1,cex=.8)})
+         col=c("black", "red","blue"),lty=1,cex=.6,pt.cex=.8)})
 dev.copy(png, file="plot3.png", width=480, height=480,units="px",bg="white")
 dev.off()
 
